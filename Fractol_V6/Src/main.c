ff186b44a49e08c9181f6c3f2796770b6a4a3a8b
Eddy Naboulet <eddy.naboulet.pro@gmail.com>
2016-05-07 16:04:47 +0200

    Update Fractol_V6/Src/main.c
    - Fractol_V6/Src/main.c
    
    Reconstructed from 42 file header Updated: 2016/05/07 16:04:47.

---
+++ Fractol_V6/Src/main.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: enaboule <enaboule@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/04/20 11:57:22 by enaboule          #+#    #+#             */
+/*   Updated: 2016/05/07 16:04:47 by enaboule         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fractol.h"
+
+int				main(int argc, char **argv)
+{
+	t_fctl		*fctl;
+
+	if (argc != 2 && argc != 3)
+		ft_help();
+	fctl = malloc(sizeof(t_fctl));
+	fctl->use = malloc(sizeof(t_use));
+	if (argc == 3)
+		ft_select_open(argv[2], fctl);
+	ft_window_size(fctl);
+	fctl->use->mlx = mlx_init();
+	fctl->use->win = mlx_new_window(fctl->use->mlx, 500, 310, "Helper");
+	ft_put_hud(fctl);
+	fctl->use->win = mlx_new_window(fctl->use->mlx, fctl->window_size,
+		fctl->window_size, fctl->window_name);
+	ft_select_fractal(argv[1], fctl);
+	ft_draw_image(fctl);
+	mlx_hook(fctl->use->win, 4, 0, ft_mouse_zoom_init, fctl);
+	mlx_hook(fctl->use->win, 2, 0, ft_init_event, fctl);
+	mlx_hook(fctl->use->win, 6, 0, ft_mouse_funct, fctl);
+	mlx_loop(fctl->use->mlx);
+}
